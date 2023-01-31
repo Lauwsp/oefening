c98d2fc18f58b1e6d4f889a16facc4ff87488979
Lauwsp <6004968@mborijnland.nl>
2023-01-31 19:41:17 +0100

    login gemaakt

--- src/Controller/Admin/ChamberCrudController.php
+++ src/Controller/Admin/ChamberCrudController.php
@@ -3,10 +3,17 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Chamber;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 
 class ChamberCrudController extends AbstractCrudController
 {
@@ -16,13 +23,40 @@ class ChamberCrudController extends AbstractCrudController
     }
 
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->addFormTheme('@FOSCKEditor/Form/ckeditor_widget.html.twig')
+            ;
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            TextField::new(propertyName: 'name'),
-            TextEditorField::new('description'),
-            MoneyField::new(propertyName: 'price')->setCurrency(currencyCode: 'EUR')
+        $filename = ImageField::new('imageName', 'Afbeelding')
+            ->setBasePath('uploads/images/chambers')
+            ->setUploadDir('public/uploads/images/chambers')
+            ->setRequired(false)
+            ->setFormTypeOptions(
+                [
+                    'required' => false,
+                ]
+            );
 
+        return [
+            TextField::new('name'),
+            /*TextEditorField::new('description'),*/
+            TextareaField::new('description')
+                ->setFormType(CKEditorType::class)
+                ->setFormTypeOptions(
+                    [
+                        'config'=>[
+                            'toolbar' => 'full',
+                        ],
+                        'attr' => ['rows' => '10'] ,
+                    ])
+                ->addCssClass('field-ck-editor')->onlyOnForms(),
+            MoneyField::new('price')->setCurrency('EUR'),
+            $filename,
         ];
     }
 
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Chamber;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -43,6 +44,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Kamer', 'fas fa-list', Chamber::class);
+        yield MenuItem::linkToCrud('User', 'fa fa-address-book', User::class);
         yield MenuItem::linkToRoute('Site', 'fas fa-arrow-left', "app_default");
     }
 }
